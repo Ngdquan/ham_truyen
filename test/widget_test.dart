4310ef97beb4560ef795c3f8ed6c290cab18e3c8
duyquan <quan.nd00192@sinhvien.hoasen.edu.vn>
2023-01-28 16:29:00 +0700

    new update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ham_truyen/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HamTruyen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
